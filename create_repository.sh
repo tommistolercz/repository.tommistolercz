d841bee72eac376548e9a71875056b880441d467
Tom Mistoler <tommistolercz@icloud.com>
2019-06-06 13:53:25 +0200

    plugin.video.vk-1.4.0.zip release

--- create_repository.sh
+++ create_repository.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# addon release
 ./create_repository.py \
-    ./_temp/repository.tommistolercz \
     ./_temp/plugin.video.vk
+
+# repository update (i.e. adding new addon)
+# ./create_repository.py \
+#     ./_temp/repository.tommistolercz \
+#     ./_temp/new.addon
